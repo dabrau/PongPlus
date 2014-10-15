f5254ec16e934bd4c0a4b70dad1e0a8eae5b482e
Darrell Abrau <darrellabrau@gmail.com>
2014-10-14 21:48:47 -0500

    ball bounce

--- PongPlus.js
+++ PongPlus.js
@@ -1,29 +1,54 @@
 var x = 5;
-	  var y = 75;
-	  var dx = 2;
-	  var dy = 2;
-    var ctx;
-  	function init(){
-  	  var canvas = document.getElementById("canvas");
-  	  ctx = canvas.getContext("2d");
-  	  return setInterval(draw,10);
-  	}
-    function draw() {
-        
+var y = 75;
+var dx = 2;
+var dy = 2;
+var paddlex;
+var paddleh;
+var paddlew;
+var ctx;
+var width = 150;
+var height = 150;
 
-      ctx.clearRect(0, 0, 150, 150);
+function init(){
+  var canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  return setInterval(draw,10);
+}
 
-      ctx.beginPath();
-      ctx.arc(x, y, 5, 0,Math.PI*2,true); // Outer circle
-      ctx.closePath();
-      ctx.fill();
-      if (x >= 150 || x <= 0) {
-      	dx *= -1;
-      } 
-      if (y >= 150 || y <= 0) {
-        dy *= -1;
-      } 
-      x += dx;
-      y += dy;
-    }
-    init();
+function circle(x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0,Math.PI*2,true); // Outer circle
+  ctx.closePath();
+  ctx.fill();
+}
+
+function init_paddle() {
+  paddlex = WIDTH / 2;
+  paddleh = 10;
+  paddlew = 75;
+}
+
+function rect(x, y, w, h) {
+  ctx.beginPath();
+  ctx.rect(x, y, w, h);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function clear() {
+  ctx.clearRect(0, 0, width, height);
+}
+function draw() {
+  clear();
+  circle(x, y, 5)
+  rect(paddlex, height-paddleh, paddlew, paddleh);
+  if (x + dx > width || x + dx < 0) {
+    dx *= -1;
+  } 
+  if (y + dy > width || y + dx < 0) {
+    dy *= -1;
+  } 
+  x += dx;
+  y += dy;
+}
+init();
