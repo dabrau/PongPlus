531d9edcd8e85b32f9c2d88ba30fb9850593a9eb
Darrell Abrau <darrellabrau@gmail.com>
2014-10-18 00:01:37 -0500

    second paddle works

--- PongPlus.js
+++ PongPlus.js
@@ -3,10 +3,17 @@ var y = 75;
 var dx = 2;
 var dy = 0;
 var ballRadius = 5;
+
 var paddle1y;
 var paddle1h;
 var paddlew;
 var paddle1sensitivity = 2
+
+var paddle2y;
+var paddle2h;
+var paddlew;
+var paddle2sensitivity = 2
+
 var ctx;
 var width = 150;
 var height = 150;
@@ -32,6 +39,12 @@ function init_paddle1() {
   paddlew = 10;
 }
 
+function init_paddle2() {
+  paddle2y = height / 2;
+  paddle2h = 50;
+  paddlew = 10;
+}
+
 function rect(x, y, w, h) {
   ctx.beginPath();
   ctx.rect(x, y, w, h);
@@ -46,27 +59,55 @@ function clear() {
 upPressed = false;
 downPressed = false;
 
+wPressed = false;
+sPressed = false;
 
-function onKeyDown(e) {
-  if (e.keyCode == 38) {
+function onP1KeyDown(e) {
+  if (e.keyCode == 87) {
     upPressed = true; 
   }
-  else if (e.keyCode == 40) {
+  else if (e.keyCode == 83) {
     downPressed = true;
   }
 }
 
-
-function onKeyUp(e) {
-  if (e.keyCode == 38) {
+function onP1KeyUp(e) {
+  if (e.keyCode == 87) {
     upPressed = false;
   }
-  else if (e.keyCode == 40) {
+  else if (e.keyCode == 83) {
     downPressed = false;
-} }
+  }   
+}
 
-$(document).keydown(onKeyDown);
-$(document).keyup(onKeyUp);
+wPressed = false;
+sPressed = false;
+
+function onP2KeyDown(e) {
+  if (e.keyCode == 38) {
+    wPressed = true; 
+  }
+  else if (e.keyCode == 40) {
+    sPressed = true;
+  }
+}
+
+function onP2KeyUp(e) {
+  if (e.keyCode == 38) {
+    wPressed = false;
+  }
+  else if (e.keyCode == 40) {
+    pPressed = false;
+  }   
+}
+
+
+$(document).keydown(onP1KeyDown);
+$(document).keyup(onP1KeyUp);
+
+$(document).keydown(onP2KeyDown);
+$(document).keyup(onP2KeyUp);
+
 function draw() {
   clear();
   circle(x, y, ballRadius)
@@ -77,12 +118,26 @@ function draw() {
     paddle1y += paddle1sensitivity;
   }
 
+    if (wPressed && paddle2y > 0) {
+    paddle2y -= paddle2sensitivity;
+  } else if (sPressed && paddle2y + paddle2h < height) {
+    paddle2y += paddle2sensitivity;
+  }
+
+  rect(width - paddlew, paddle2y , paddlew, paddle2h);
   rect(0, paddle1y , paddlew, paddle1h);
   if (y + dy > height || y + dy < 0) {
     dy = -dy;
   } 
-  if (x + dx > height)
-    dx = -dx;
+  if (x + dx > height) {
+    if (y > paddle2y && y < paddle2y + paddle1h) {
+      dy = 1 * ((y-(paddle2y+paddle2h/2))/paddlew);
+      dx = -dx;
+    } else {
+      clearInterval(intervalId);
+      alert("Thanks for playing!")
+    }
+  }
   else if (x + dx < 0) {
     if (y > paddle1y && y < paddle1y + paddle1h) {
       dy = 1 * ((y-(paddle1y+paddle1h/2))/paddlew);
@@ -98,3 +153,4 @@ function draw() {
 }
 init();
 init_paddle1();
+init_paddle2();
